8584430909dafc3938bb4d9e5912ed39487574ab
OZAN CAN KOSEMEZ <ozancank@gmail.com>
2021-08-20 15:20:30 +0300

    .

--- routing/app.js
+++ routing/app.js
@@ -1,5 +1,6 @@
 const http = require("http");
 const fs = require("fs");
+const qs = require("querystring");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
@@ -24,14 +25,25 @@ const server = http.createServer((req, res) => {
   }
 
   if (url === "/log" && method === "POST") {
-    req.on("data", function (message) {
-      fs.appendFileSync("message.txt", `\n${message.slice(8)}`);
-    });    
-    req.on("end", function () {
-      res.statusCode = 302;
-      res.setHeader("Location", "/");
-      return res.end();
+    const body = [];
+
+    req.on("data", (chunk) => {
+      body.push(chunk);
+      console.log(1,chunk);
     });
+
+    req.on("end", () => {
+      const bodyParsed = Buffer.concat(body).toString();
+      const message = bodyParsed.split("=")[1];
+
+      console.log(2,qs.parse(bodyParsed));
+
+      fs.appendFileSync("message.txt", `\n${message}`);
+    });
+
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
   }
 });
 
